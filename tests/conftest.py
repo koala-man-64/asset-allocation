e4d9e49215bfc0fc2f633cc48fce99d393cd5d7b
rdprokes <rdprokes@gmail.com>
2026-01-04 17:16:07 -0600

    fixed config

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,28 @@ import sys
 # Add project root to sys.path if not picked up by pythonpath
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+# Mock Environment Variables for Testing (Must be set BEFORE importing config)
+if "YAHOO_USERNAME" not in os.environ:
+    os.environ["YAHOO_USERNAME"] = "test_user"
+if "YAHOO_PASSWORD" not in os.environ:
+    os.environ["YAHOO_PASSWORD"] = "test_password"
+if "AZURE_STORAGE_ACCOUNT_NAME" not in os.environ:
+    os.environ["AZURE_STORAGE_ACCOUNT_NAME"] = "test_account"
+if "AZURE_STORAGE_CONNECTION_STRING" not in os.environ:
+    os.environ["AZURE_STORAGE_CONNECTION_STRING"] = "DefaultEndpointsProtocol=https;AccountName=test;AccountKey=key;EndpointSuffix=core.windows.net"
+if "HEADLESS_MODE" not in os.environ:
+    os.environ["HEADLESS_MODE"] = "True"
+
+# Container Mocks
+containers = [
+    "AZURE_CONTAINER_MARKET", "AZURE_CONTAINER_FINANCE", 
+    "AZURE_CONTAINER_EARNINGS", "AZURE_CONTAINER_TARGETS", 
+    "AZURE_CONTAINER_COMMON"
+]
+for container in containers:
+    if container not in os.environ:
+        os.environ[container] = "test-container"
+
 from scripts.common import config as cfg
 from scripts.common.blob_storage import BlobStorageClient
 
